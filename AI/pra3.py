c451024b0677f9c62c21e486496c02511c9c2389
Prajwal kadam <prajwalkadam3208@gmail.com>
2024-05-02 09:17:28 +0530

    Update pra3.py

--- AI/pra3.py
+++ AI/pra3.py
@@ -1,22 +1,23 @@
 #selection sort
-def selectionSort(array, size):
-    for ind in range(size):
-        min_index = ind
+def sort(l):
+    size=len(l)
+    for i in range(size-1):
+        min=i
+        for j in range(i+1,size):
+            if l[j] < l[min]:
+                min=j
+        l[i],l[min]=l[min],l[i]
+        print(l)
 
-        for j in range(ind + 1, size):
-            if array[j] < array[min_index]:
-                min_index = j
-
-        array[ind], array[min_index] = array[min_index], array[ind]
-
-
-arr = list(map(int, input("Enter elements of the array separated by space: ").split()))
-
-size = len(arr)
-selectionSort(arr, size)
-
-print('The array after sorting in Ascending Order by selection sort is:')
-print(arr)
+def main():
+    l=[]
+    n = int(input("Enter no of students: "))
+    for i in range(0, n):
+        k = int(input("Enter percentage: "))
+        l.append(k)
+    print(l)
+    sort(l)
+main()
 
 #krushkal
 class Graph:
